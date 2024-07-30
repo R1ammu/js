468945baf61bafe77414029a52e60172021ece22
Liam <171575208+R1ammu@users.noreply.github.com>
2024-07-30 09:42:23 +0200

    project sum on a webpage instead of consol.log

--- FizzBuzz/script.js
+++ FizzBuzz/script.js
@@ -30,3 +30,20 @@ let int = 0;
 }
 
 numbers(userNumber);
+
+
+
+// FROM TOP
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
