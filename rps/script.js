ca8a8fee16fa132e53d5f2fc06ef96baa8b51462
Liam <171575208+R1ammu@users.noreply.github.com>
2024-08-01 14:56:13 +0200

    function 'getRandomInt' finished

---
+++ rps/script.js
@@ -0,0 +1,9 @@
+function getRandomInt() {
+    const min = 1;
+    const max = 3;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(getRandomInt())
+function getComputerChoice() {
+    
+}
