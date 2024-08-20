850e8c21314bfb630182e9a5c4c041ecdcfe1787
liam <171575208+R1ammu@users.noreply.github.com>
2024-08-20 14:28:21 +0200

    .

---
+++ sudoku/functions.js
@@ -0,0 +1,35 @@
+const ints = [1,2,3,4,5,6,7,8,9]
+
+const row1 = new Array(9);
+const row2 = new Array(9);
+const row3 = new Array(9);
+const row4 = new Array(9);
+const row5 = new Array(9);
+const row6 = new Array(9);
+const row7 = new Array(9);
+const row8 = new Array(9);
+const row9 = new Array(9);
+
+
+
+function createRow () {
+    
+}
+
+function createBoard () {   
+
+}
+
+
+
+function chooseInt (condition) {
+    if (condition === 1) return 1;
+    else if (condition === 2) return 2;
+    else if (condition === 3) return 3;
+    else if (condition === 4) return 4;
+    else if (condition === 5) return 5;
+    else if (condition === 6) return 6;
+    else if (condition === 7) return 7;
+    else if (condition === 8) return 8;
+    else return 9;
+}
